191c23b3e6472ef27d0cf7ab3ca19f61572c607a
Lauren <46353764+ljtbautista@users.noreply.github.com>
2020-11-04 09:38:51 -0600

    cards on homepage flippity flip

---
+++ Controllers/BigIdeasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BJC_Project.Controllers
+{
+    public class BigIdeasController : Controller
+    {
+        //add other big idea pages here if were gonna do them
+        public ActionResult Programming()
+        {
+            return View();
+        }
+    }
+}
