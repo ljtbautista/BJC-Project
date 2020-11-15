eb42d83ab42ba7291a2444007c38978c8b18f1a4
ocshephard <47821925+ocshephard@users.noreply.github.com>
2020-11-15 14:28:45 -0600

    test completed

---
+++ Content/BigIdeas/JavascriptTest/JavaScriptTest.js
@@ -0,0 +1,21 @@
+﻿$("#done").click(function () {
+    var correctAnswers = 0;
+    if ($('input[name="opt1radio"]:checked').hasClass("correct")) correctAnswers++;
+    if ($('input[name="opt2radio"]:checked').hasClass("correct")) correctAnswers++;
+    if ($('input[name="opt3radio"]:checked').hasClass("correct")) correctAnswers++;
+    if ($('input[name="opt4radio"]:checked').hasClass("correct")) correctAnswers++;
+    if ($('input[name="opt5radio"]:checked').hasClass("correct")) correctAnswers++;
+    $("#percentCorrect").html((correctAnswers * 20) + "%");
+    if (correctAnswers == 5) {
+        $("#tryagain").css("display", "none");
+        $("#tryAgainText").css("display", "none");
+        $("#badgePic").css("display", "block");
+        $("#badgeText").css("display", "block");
+    } else {
+        $("#tryagain").css("display", "block");
+        $("#tryAgainText").css("display", "block");
+        $("#badgePic").css("display", "none");
+        $("#badgeText").css("display", "none");
+    }
+});
+
